6ae827c80d1992c7504422b171e201f4be5e785b
MANISH PRAJAPATI <manishpra219@gmail.com>
2024-06-14 22:11:03 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["*"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+
+
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {   
+      colors: {
+        custom: {
+          
+          brand: '#404EED',
+          brand1: '#f6f6f6',
+          foot:'#23272a',
+          
+        },
+      },
+    },
+  },
+};
+
